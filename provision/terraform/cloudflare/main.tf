16b1e832ae8bada148091d346f2832780a71c216
Irvan Tristian <irvan.tristian@tokopedia.com>
2022-11-24 07:59:02 +0700

    feat: add new endpoint for vaultwarden

--- provision/terraform/cloudflare/main.tf
+++ provision/terraform/cloudflare/main.tf
@@ -136,8 +136,8 @@ resource "cloudflare_record" "echo_server" {
   ttl     = 1
 }
 
-resource "cloudflare_record" "rass" {
-  name    = "rass"
+resource "cloudflare_record" "vault" {
+  name    = "vault"
   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
   value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
   proxied = true
