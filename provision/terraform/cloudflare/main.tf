5152c0cafd024dd1d9ceb6c40ed1cc70e4c20380
Irvan Tristian <irvan@tristian.id>
2023-04-20 22:22:51 +0700

    feat: Add casper endpoint

--- provision/terraform/cloudflare/main.tf
+++ provision/terraform/cloudflare/main.tf
@@ -208,6 +208,15 @@ resource "cloudflare_record" "oracle_remote_subdomain" {
   ttl     = 1
 }
 
+resource "cloudflare_record" "oracle_casper_subdomain" {
+  name    = data.sops_file.cloudflare_secrets.data["oracle_casper_subdomain"]
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = data.sops_file.cloudflare_secrets.data["cname_oracle_domain"]
+  proxied = true
+  type    = "CNAME"
+  ttl     = 1
+}
+
 # resource "cloudflare_record" "home_subdomain" {
 #   name    = data.sops_file.cloudflare_secrets.data["home_subdomain"]
 #   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
