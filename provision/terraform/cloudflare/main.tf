5c8fe95f52fd6fdc4e58e926d2f236177442118e
Irvan Tristian <irvan@tristian.id>
2023-04-21 10:49:43 +0700

    fix: Expose home-assistant endpoint

--- provision/terraform/cloudflare/main.tf
+++ provision/terraform/cloudflare/main.tf
@@ -217,14 +217,14 @@ resource "cloudflare_record" "oracle_casper_subdomain" {
   ttl     = 1
 }
 
-# resource "cloudflare_record" "home_subdomain" {
-#   name    = data.sops_file.cloudflare_secrets.data["home_subdomain"]
-#   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
-#   value   = data.sops_file.cloudflare_secrets.data["cname_home_domain"]
-#   proxied = true
-#   type    = "CNAME"
-#   ttl     = 1
-# }
+resource "cloudflare_record" "home_subdomain" {
+  name    = data.sops_file.cloudflare_secrets.data["home_subdomain"]
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = data.sops_file.cloudflare_secrets.data["cname_home_domain"]
+  proxied = true
+  type    = "CNAME"
+  ttl     = 1
+}
 
 # resource "cloudflare_record" "weave_subdomain" {
 #   name    = data.sops_file.cloudflare_secrets.data["weave_subdomain"]
