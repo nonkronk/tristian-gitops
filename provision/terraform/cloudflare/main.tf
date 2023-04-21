ac22e57d02658bfdc8b18531c18f932442f7c3db
Irvan Tristian <irvan@tristian.id>
2023-04-21 18:44:36 +0700

    chore: Unexpose container service

--- provision/terraform/cloudflare/main.tf
+++ provision/terraform/cloudflare/main.tf
@@ -271,14 +271,14 @@ resource "cloudflare_record" "home_subdomain" {
 #   ttl     = 1
 # }
 
-resource "cloudflare_record" "container_subdomain" {
-  name    = data.sops_file.cloudflare_secrets.data["container_subdomain"]
-  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
-  value   = data.sops_file.cloudflare_secrets.data["cname_home_domain"]
-  proxied = true
-  type    = "CNAME"
-  ttl     = 1
-}
+# resource "cloudflare_record" "container_subdomain" {
+#   name    = data.sops_file.cloudflare_secrets.data["container_subdomain"]
+#   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+#   value   = data.sops_file.cloudflare_secrets.data["cname_home_domain"]
+#   proxied = true
+#   type    = "CNAME"
+#   ttl     = 1
+# }
 
 resource "cloudflare_record" "indie_subdomain" {
   name    = data.sops_file.cloudflare_secrets.data["indie_subdomain"]
