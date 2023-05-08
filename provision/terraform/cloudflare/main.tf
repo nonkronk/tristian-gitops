556477f7bd8e22c75aaf4368fbd21fc9d7d69f67
Irvan Tristian <irvan@tristian.id>
2023-05-09 01:01:45 +0700

    feat: Add subdomain for node svc

--- provision/terraform/cloudflare/main.tf
+++ provision/terraform/cloudflare/main.tf
@@ -423,3 +423,12 @@ resource "cloudflare_record" "backside_subdomain" {
   type    = "CNAME"
   ttl     = 1
 }
+
+resource "cloudflare_record" "node_subdomain" {
+  name    = data.sops_file.cloudflare_secrets.data["node_subdomain"]
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = data.sops_file.cloudflare_secrets.data["cname_home_domain"]
+  proxied = true
+  type    = "CNAME"
+  ttl     = 1
+}
