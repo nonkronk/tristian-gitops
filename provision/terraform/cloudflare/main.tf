501d4bb50ef5dc4b0f0dd31f9d92f1e23101934f
Irvan Tristian <irvan@tristian.id>
2023-08-06 11:09:13 +0700

    feat: Add new subdomain for 'micro' oracle svc

--- provision/terraform/cloudflare/main.tf
+++ provision/terraform/cloudflare/main.tf
@@ -226,6 +226,15 @@ resource "cloudflare_record" "oracle_kasm_subdomain" {
   ttl     = 1
 }
 
+resource "cloudflare_record" "oracle_micro_subdomain" {
+  name    = data.sops_file.cloudflare_secrets.data["oracle_micro_subdomain"]
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = data.sops_file.cloudflare_secrets.data["cname_oracle_domain"]
+  proxied = true
+  type    = "CNAME"
+  ttl     = 1
+}
+
 resource "cloudflare_record" "home_subdomain" {
   name    = data.sops_file.cloudflare_secrets.data["home_subdomain"]
   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
