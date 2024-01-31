a9c49df29af102a1efc221077e7c3cdacc2ed459
Irvan Tristian <irvan@tristian.id>
2024-01-31 17:12:23 +0700

    feat: add tristian detection svc subdomain

--- provision/terraform/cloudflare/main.tf
+++ provision/terraform/cloudflare/main.tf
@@ -235,6 +235,15 @@ resource "cloudflare_record" "oracle_micro_subdomain" {
   ttl     = 1
 }
 
+resource "cloudflare_record" "oracle_change_subdomain" {
+  name    = data.sops_file.cloudflare_secrets.data["oracle_change_subdomain"]
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = data.sops_file.cloudflare_secrets.data["cname_oracle_domain"]
+  proxied = true
+  type    = "CNAME"
+  ttl     = 1
+}
+
 resource "cloudflare_record" "home_subdomain" {
   name    = data.sops_file.cloudflare_secrets.data["home_subdomain"]
   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
