99e364e2a987a5f25773051c9f44f7bd186911de
irvan@tristian.id <irvan@tristian.id>
2024-11-09 05:14:09 +0000

    feat: add couch svc endpoint

--- provision/terraform/cloudflare/main.tf
+++ provision/terraform/cloudflare/main.tf
@@ -254,6 +254,15 @@ resource "cloudflare_record" "oracle_ping_subdomain" {
   ttl     = 1
 }
 
+resource "cloudflare_record" "oracle_couch_subdomain" {
+  name    = data.sops_file.cloudflare_secrets.data["oracle_couch_subdomain"]
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = data.sops_file.cloudflare_secrets.data["cname_oracle_domain"]
+  proxied = true
+  type    = "CNAME"
+  ttl     = 1
+}
+
 resource "cloudflare_record" "home_subdomain" {
   name    = data.sops_file.cloudflare_secrets.data["home_subdomain"]
   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
